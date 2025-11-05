705defc5d5d5482149a57846d92935e6f90855b8
Mônica Santos Costa <monica.costa.27793@formandos.cinel.pt>
2025-11-05 22:22:17 +0000

    API products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    public class ProductsController : Controller
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+
+        public class ProductController : Controller
+        {
+            private readonly IProductRepository _productRepository;
+
+            public ProductController(IProductRepository productRepository)
+            {
+                _productRepository = productRepository;
+            }
+
+            public IActionResult GetProducts()
+            {
+                return View();
+            }
+        }
+    }
+}
