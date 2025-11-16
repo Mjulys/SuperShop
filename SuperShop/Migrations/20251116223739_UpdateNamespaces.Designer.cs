c2b989f1ed6f54c6dcc7a9949d30eaea17ef39a4
Mônica Santos Costa <monica.costa.27793@formandos.cinel.pt>
2025-11-16 22:37:42 +0000

    Corrigir erros de Views e criar migration UpdateNamespaces

--- SuperShop/Migrations/20251116223739_UpdateNamespaces.Designer.cs
+++ SuperShop/Migrations/20251116223739_UpdateNamespaces.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Supershop.Data;
 
 namespace Supershop.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20251116223739_UpdateNamespaces")]
+    partial class UpdateNamespaces
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
